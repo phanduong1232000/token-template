c3a8530f144995b821278bf291c8780612fe04e5
phanduong1232000 <duongptps2000@gmail.com>
2023-02-03 10:17:38 +0700

    first commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,6 +7,102 @@
         $body_m.addClass("no-touch");
     }
 
+
+    // add img to select
+    var langArray = [];
+    $('.vodiapicker option').each(function () {
+        var img = $(this).attr("data-thumbnail");
+        var text = this.innerText;
+        var value = $(this).val();
+        var item = '<li><img src="' + img + '" alt="" value="' + value + '"/><span>' + text + '</span></li>';
+        langArray.push(item);
+    })
+
+    $('#a').html(langArray);
+
+    //Set the button value to the first el of the array
+    $('.btn-select').html(langArray[0]);
+    $('.btn-select').attr('value', 'en');
+
+    //change button stuff on click
+    $('#a li').click(function () {
+        var img = $(this).find('img').attr("src");
+        var value = $(this).find('img').attr('value');
+        var text = this.innerText;
+        var item = '<li><img src="' + img + '" alt="" /><span>' + text + '</span></li>';
+        $('.btn-select').html(item);
+        $('.btn-select').attr('value', value);
+        $(".b").toggle();
+        //console.log(value);
+    });
+
+    $(".btn-select").click(function () {
+        $(".b").toggle();
+    });
+
+    //check local storage for the lang
+    var sessionLang = localStorage.getItem('lang');
+    if (sessionLang) {
+        //find an item with value of sessionLang
+        var langIndex = langArray.indexOf(sessionLang);
+        $('.btn-select').html(langArray[langIndex]);
+        $('.btn-select').attr('value', sessionLang);
+    } else {
+        var langIndex = langArray.indexOf('ch');
+        console.log(langIndex);
+        $('.btn-select').html(langArray[langIndex]);
+        //$('.btn-select').attr('value', 'en');
+    }
+
+
+    // add img to select ==========ONE===========
+    var langArray = [];
+    $('.vodiapicker-one option').each(function () {
+        var img = $(this).attr("data-thumbnail");
+        var text = this.innerText;
+        var value = $(this).val();
+        var item = '<li><img src="' + img + '" alt="" value="' + value + '"/><span>' + text + '</span></li>';
+        langArray.push(item);
+    })
+
+    $('#a-one').html(langArray);
+
+    //Set the button value to the first el of the array
+    $('.btn-select-one').html(langArray[0]);
+    $('.btn-select-one').attr('value', 'en');
+
+    //change button stuff on click
+    $('#a-one li').click(function () {
+        var img = $(this).find('img').attr("src");
+        var value = $(this).find('img').attr('value');
+        var text = this.innerText;
+        var item = '<li><img src="' + img + '" alt="" /><span>' + text + '</span></li>';
+        $('.btn-select-one').html(item);
+        $('.btn-select-one').attr('value', value);
+        $(".b-one").toggle();
+        //console.log(value);
+    });
+
+    $(".btn-select-one").click(function () {
+        $(".b-one").toggle();
+    });
+
+    //check local storage for the lang
+    var sessionLang = localStorage.getItem('lang');
+    if (sessionLang) {
+        //find an item with value of sessionLang
+        var langIndex = langArray.indexOf(sessionLang);
+        $('.btn-select-one').html(langArray[langIndex]);
+        $('.btn-select-one').attr('value', sessionLang);
+    } else {
+        var langIndex = langArray.indexOf('ch');
+        console.log(langIndex);
+        $('.btn-select-one').html(langArray[langIndex]);
+        //$('.btn-select').attr('value', 'en');
+    }
+
+    // end select img
+
     function winwidth() {
         return $win.width();
     }
@@ -306,4 +402,6 @@
             $pr_tg.addClass('active');
         }
     });
+
+
 })(jQuery);
